acad13253599fcaa074cdfe9ee0c8d9200bce5e9
Akhil Rasamalla <akhilrasamalla0805@gmail.com>
2026-05-11 13:26:08 +0530

    hardcoded socket url

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const SOCKET_URL = "https://typenova-backend-on7p.onrender.com";
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
+  withCredentials: true,
 });
 
 export default socket;
