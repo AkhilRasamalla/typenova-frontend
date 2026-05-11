a2f6b5e2cb01ccfd62a0db6866215ea180ace1f1
Akhil Rasamalla <akhilrasamalla0805@gmail.com>
2026-05-11 11:52:44 +0530

    fixed socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
-const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
-
-const socket = io(SOCKET_URL,{ transports: ["websocket"] });
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+});
 
 export default socket;
